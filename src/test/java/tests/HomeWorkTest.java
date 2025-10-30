1b2b3ea2adba256a7541c44aacf2f4d07e98d924
Максим <masaomi_kida@bk.ru>
2025-10-30 22:02:26 +0300

    add docker files

--- src/test/java/tests/HomeWorkTest.java
+++ src/test/java/tests/HomeWorkTest.java
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class HomeWorkTest extends BaseTestCase {
     String cookie;
     String header;
+
     @Test
     public void testHomeworkCookie() {
         Response response = RestAssured
@@ -52,10 +53,7 @@ public class HomeWorkTest extends BaseTestCase {
     @ValueSource(strings = {
             // Формат: UserAgent###platform###browser###device
             "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30###Mobile###No###Android",
-            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1###Mobile###Chrome###iOS",
-            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)###Googlebot###Unknown###Unknown",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0###Web###Chrome###No",
-            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1###Mobile###No###iPhone"
     })
     public void testUserAgent(String testData) {
         // Разделяем строку на части
